08504e960eaaf2c1ae6ca4ec81c7943dc432a128
mafumaful <s6bian@uwaterloo.ca>
2023-02-12 15:55:55 -0500

    default

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo apt install libcairo2-dev ffmpeg \
     texlive texlive-latex-extra texlive-fonts-extra \
     texlive-latex-recommended texlive-science \
     tipa libpango1.0-dev
-pip install manim moviepy manim_physics
+pip install manim moviepy manim_physics scipy
 pip install IPython --upgrade
